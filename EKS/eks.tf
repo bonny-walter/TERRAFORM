ab3f8d1106086627c3ff9540b93fb090ea99f89f
bonny-walter <jofranco1203@gmail.com>
2024-10-17 22:59:49 +0200

    update

--- EKS/eks.tf
+++ EKS/eks.tf
@@ -1,4 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-2"
+}
 
 # cluster resource
 resource "aws_eks_cluster" "eks-cluster" {
